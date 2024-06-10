bee88d27b7765b462e1bc10a38116e95d228a3d5
Aholiche <bryan92882@gmail.com>
2024-06-10 10:24:05 -0700

    Update SaveLoadData.cs

--- Assets/Scripts 4 Final/SaveLoad Data/SaveLoadData.cs
+++ Assets/Scripts 4 Final/SaveLoad Data/SaveLoadData.cs
@@ -5,7 +5,7 @@ using Newtonsoft.Json;
 
 public class SaveLoadData : MonoBehaviour
 {
-    //My attempt to connect the buttons to a saving/loading script to save an instance of a scene. For somre reason, it says the json lubrary is unnecessary, but when using json
+    //My attempt to connect the buttons to a saving/loading script to save an instance of a scene. For somre reason, it says the json library is unnecessary, but when using json
     //syntax, it doesn't exist. Maybe it was declared incorrectly. 
     public void Initialized()
     {
